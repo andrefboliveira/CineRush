1c15e315b19c8f914a56cb785a3c15a5aebcc811
André Oliveira <andrefboliveira@users.noreply.github.com>
2016-11-07 00:05:35 +0000

    Adicionado SDK para login com Facebook e Google

--- app/src/main/java/pt/ulisboa/ciencias/cinerush/LoginActivity.java
+++ app/src/main/java/pt/ulisboa/ciencias/cinerush/LoginActivity.java
@@ -2,6 +2,9 @@ package pt.ulisboa.ciencias.cinerush;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        //AppEventsLogger.activateApp(this);
     }
 }
