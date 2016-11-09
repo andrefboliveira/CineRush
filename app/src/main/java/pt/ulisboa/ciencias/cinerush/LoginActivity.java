1fdc0623fe9a4aebbe5ec4a7207689e3acbd9d20
Peixinho88 <tom.peixinho@hotmail.com>
2016-11-09 15:25:33 +0000

    alteraçoes no login_activity (nao funcionam)

--- app/src/main/java/pt/ulisboa/ciencias/cinerush/LoginActivity.java
+++ app/src/main/java/pt/ulisboa/ciencias/cinerush/LoginActivity.java
@@ -2,6 +2,10 @@ package pt.ulisboa.ciencias.cinerush;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
@@ -14,5 +18,22 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         FacebookSdk.sdkInitialize(getApplicationContext());
         //AppEventsLogger.activateApp(this);
+
+        if(!isLoggedIn()){
+            Button myButton = new Button(this);
+            myButton.setText("Push Me");
+
+            LinearLayout ll = (LinearLayout)findViewById(R.id.buttonlayout);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            ll.addView(myButton, lp);
+        }
+        else{
+
+        }
+    }
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
     }
 }
